b6aa26be3ddb48d5b1de2f5693ba95703fe5bd6c
Adi <adeline.huvet.emploi@gmail.com>
2022-06-21 17:37:35 +0200

    Mise en place du update et modification create

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -13,56 +13,86 @@ use Container0jRK7Ab\PaginatorInterface_82dac15;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\{TextType,ButtonType,EmailType,HiddenType,PasswordType,TextareaType,SubmitType,NumberType,DateType,MoneyType,BirthdayType};
+use Symfony\Component\Form\Extension\Core\Type\{TextType, ButtonType, EmailType, HiddenType, PasswordType, TextareaType, SubmitType, NumberType, DateType, MoneyType, BirthdayType};
 
 class IngredientController extends AbstractController
 {
-     /**
-      * This function display all ingredients
-      *
-      * @param IngredientRepository $repository
-      * @param PaginatorInterface $paginator
-      * @param Request $request
-      * @return Response
-      */
+    /**
+     * This function display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
 
     #[Route('/ingredient', name: 'ingredient', methods: ['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(
             $repository->findAll(),
-            $request->query->getInt('page', 1), 
+            $request->query->getInt('page', 1),
             10
-        ); 
-   
+        );
+
         return $this->render('pages/ingredient/index.html.twig', [
             'ingredients' => $ingredients
-            
+
         ]);
     }
 
     #[Route('/ingredient/nouveau', name: 'ingredient.new', methods: ['GET', 'POST'])]
-    public function new( Request $request, EntityManagerInterface $manager) : Response
+    public function new(Request $request, EntityManagerInterface $manager): Response
     {
         $ingredient = new Ingredient();
         $form = $this->createForm(IngredientType::class, $ingredient);
 
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-                $ingredient = $form->getData();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
 
-               $manager->persist($ingredient);
-               $manager->flush();
+            $manager->persist($ingredient);
+            $manager->flush();
 
-               $this->addFlash(
-                   'success',
-                   'Votre ingrédient à été crée avec succès !',
-               );  
-               return $this->redirectToRoute('ingredient');  
+            $this->addFlash(
+
+                'success',
+                'Votre ingrédient à été crée avec succès !',
+            );
+            return $this->redirectToRoute('ingredient');
         }
 
         return $this->render('pages/ingredient/new.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    #[Route('ingredient/edition/{id}', 'ingredient.edit', methods: ['GET','POST'])]
+    public function edit(Ingredient $ingredient, Request $request, EntityManagerInterface $manager): Response
+    {
+
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash(
+
+                'success',
+                'Votre ingrédient à été modifié avec succès !',
+            );
+            return $this->redirectToRoute('ingredient');
+        }
+
+        return $this->render(
+            'pages/ingredient/edit.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
     }
 }
